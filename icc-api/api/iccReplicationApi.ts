8d4fa23297851d9e631291aaa5f24ec0275c42ef
julien wathelet <julien.wathelet@hotmail.be>
2021-03-03 12:21:55 +0100

    correction Replication + invoice item correction

--- icc-api/api/iccReplicationApi.ts
+++ icc-api/api/iccReplicationApi.ts
@@ -47,85 +47,104 @@ export class iccReplicationApi {
 
   createGroupReplication(replicationHost: string, groupId: string, password: string, protocol?: string, port?: string): Promise<models.ReplicationDto | any> {
     let _body = null
-    
-    const _url = this.host + "/replication/group/{replicationHost}/{groupId}/{password}".replace("{replicationHost}", replicationHost+"").replace("{groupId}", groupId+"").replace("{password}", password+"") + "?ts=" + new Date().getTime()  + (protocol ? "&protocol=" + protocol : "") + (port ? "&port=" + port : "")
+
+    const _url = this.host + "/replication/group/{replicationHost}/{groupId}/{password}".replace("{replicationHost}", replicationHost + "").replace("{groupId}", groupId + "").replace("{password}", password + "") + "?ts=" + new Date().getTime() + (protocol ? "&protocol=" + protocol : "") + (port ? "&port=" + port : "")
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("POST", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.ReplicationDto(doc.body as JSON))
+      .then(doc => new models.ReplicationDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+  }
+
   createReplication(body?: models.ReplicationDto): Promise<models.AccessLogDto | any> {
     let _body = null
     _body = body
-    
-    const _url = this.host + "/replication" + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication" + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("POST", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.AccessLogDto(doc.body as JSON))
+      .then(doc => new models.AccessLogDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+  }
+
   createStandardReplication(replicationHost: string): Promise<models.AccessLogDto | any> {
     let _body = null
-    
-    const _url = this.host + "/replication/standard/{replicationHost}".replace("{replicationHost}", replicationHost+"") + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication/standard/{replicationHost}".replace("{replicationHost}", replicationHost + "") + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("POST", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.AccessLogDto(doc.body as JSON))
+      .then(doc => new models.AccessLogDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+  }
+
   createTemplateReplication(replicationHost: string, language: string, specialtyCode: string, protocol?: string, port?: string): Promise<models.ReplicationDto | any> {
     let _body = null
-    
-    const _url = this.host + "/replication/template/{replicationHost}/{language}/{specialtyCode}".replace("{replicationHost}", replicationHost+"").replace("{language}", language+"").replace("{specialtyCode}", specialtyCode+"") + "?ts=" + new Date().getTime()  + (protocol ? "&protocol=" + protocol : "") + (port ? "&port=" + port : "")
+
+    const _url = this.host + "/replication/template/{replicationHost}/{language}/{specialtyCode}".replace("{replicationHost}", replicationHost + "").replace("{language}", language + "").replace("{specialtyCode}", specialtyCode + "") + "?ts=" + new Date().getTime() + (protocol ? "&protocol=" + protocol : "") + (port ? "&port=" + port : "")
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("POST", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.ReplicationDto(doc.body as JSON))
+      .then(doc => new models.ReplicationDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+  }
+
   deleteReplication(replicationId: string): Promise<any | Boolean> {
     let _body = null
-    
-    const _url = this.host + "/replication/{replicationId}".replace("{replicationId}", replicationId+"") + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication/{replicationId}".replace("{replicationId}", replicationId + "") + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("DELETE", _url, headers, _body, this.fetchImpl)
       .then(doc => (true))
       .catch(err => this.handleError(err))
-}
+  }
+
   getReplication(replicationId: string): Promise<models.ReplicationDto | any> {
     let _body = null
-    
-    const _url = this.host + "/replication/{replicationId}".replace("{replicationId}", replicationId+"") + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication/{replicationId}".replace("{replicationId}", replicationId + "") + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("GET", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.ReplicationDto(doc.body as JSON))
+      .then(doc => new models.ReplicationDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+  }
+
   listReplications(): Promise<Array<models.ReplicationDto> | any> {
     let _body = null
-    
-    const _url = this.host + "/replication" + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication" + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("GET", _url, headers, _body, this.fetchImpl)
       .then(doc => (doc.body as Array<JSON>).map(it => new models.ReplicationDto(it)))
       .catch(err => this.handleError(err))
-}
+  }
+
   modifyReplication(body?: models.ReplicationDto): Promise<models.ReplicationDto | any> {
     let _body = null
     _body = body
-    
-    const _url = this.host + "/replication" + "?ts=" + new Date().getTime() 
+
+    const _url = this.host + "/replication" + "?ts=" + new Date().getTime()
     let headers = this.headers
     headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
     return XHR.sendCommand("PUT", _url, headers, _body, this.fetchImpl)
-      .then(doc =>  new models.ReplicationDto(doc.body as JSON))
+      .then(doc => new models.ReplicationDto(doc.body as JSON))
       .catch(err => this.handleError(err))
-}
+
+  }
+
+  startTransientReplication(body?: models.ReplicationDto): Promise<models.AccessLogDto | any> {
+    let _body = null
+    _body = body
+    const _url = this.host + "/replication/transient" + "?ts=" + new Date().getTime()
+    let headers = this.headers
+    headers = headers.filter(h => h.header !== "Content-Type").concat(new XHR.Header("Content-Type", "application/json"))
+    return XHR.sendCommand("POST", _url, headers, _body, this.fetchImpl)
+      .then(doc => new models.AccessLogDto(doc.body as JSON))
+      .catch(err => this.handleError(err))
+  }
 }
 
